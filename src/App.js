286cd34afc803a9aea865dc2a9a1e587169dd45e
flor90200 <flor90200@gmail.com>
2023-05-23 18:32:27 -0300

    proyecto

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import  './style.css'
 import Navbar from './components/Navbar';
+import ItemListContainer from './components/ItemListContainer'
+import ItemDetailContainer from './components/ItemDetailContainer'
+
 
 function App (){
 
     return(
         <div>
            <Navbar/>
+           { <ItemListContainer/> }
+           <ItemDetailContainer itemId={2}/>
         </div>
     );
 }
--- src/components/Item.jsx
+++ src/components/Item.jsx
@@ -1,9 +1,21 @@
 import React from "react";
 
-const Item = () =>{
+const Item = (producto) =>{
     return (
-    <div>Item</div>
+   
+   <div className="producto">
+       <img src={producto.img}/>
+        <div>
+        <h4>{producto.titulo}</h4>
+        <p> Precio: ${producto.precio}</p>
+        <p> Categoria: {producto.categoria}</p>
+        <p>{producto.descripcion}</p>
+        <a className="ver-mas" href={`/item/${producto.id}`}>ver-mas</a>
+    </div>
+    </div>
     )
 }
 
 export default Item
+
+
