229ee55c608695898167a07527de7ef025384b83
flor90200 <flor90200@gmail.com>
2023-05-24 01:29:17 -0300

    correccion

--- src/components/ItemDetail.jsx
+++ src/components/ItemDetail.jsx
@@ -1,4 +1,18 @@
+import { useState } from "react";
+import ItemCount from "./ItemCount"
+import {toCapital} from './helpers/toCapital'
 const ItemDetail = ( {item} )=>{
+    const  [cantidad, setCantidad] = useState(1);
+   const handleRestar = ()=>{
+    cantidad >1 && setCantidad(cantidad -1)
+   }
+   const handleSumar =()=>{
+   cantidad < item.stock && setCantidad(cantidad +1)
+   }
+   const handleAgregar = ()=>{
+    console.log({...item, quantity: cantidad});
+   }
+
     return(
         <div className="container">
             <div className="producto-detalle">
@@ -6,8 +20,9 @@ const ItemDetail = ( {item} )=>{
             <div>
                 <h3 className="titulo">{item.titulo}</h3>
                 <p className="descripcion">{item.descripcion}</p>
-                <p className="categoria">Categoria: {item.categoria}</p>
+                <p className="categoria">Categoria: {toCapital(item.categoria)}</p>
                 <p className="precio">${item.precio}</p>
+                <ItemCount cantidad={cantidad} handleSumar={handleSumar} handleRestar={handleRestar} handleAgregar={handleAgregar}/>
             </div>
             </div>
         </div>
---
+++ src/components/Nosotros.jsx
@@ -0,0 +1,27 @@
+import { click } from "@testing-library/user-event/dist/click";
+import React, { useEffect } from "react";
+
+const Nosotros = () =>{
+
+    useEffect(()=>{
+        const clickear = ()=>{
+            console.log("click");
+        }
+
+       window.addEventListener("click", clickear)
+    
+    return()=>{
+        window.removeEventListener("click", clickear)
+        }
+    
+    }, [] )
+    
+    return(
+        <div className="container">
+            <h1 className="main-title">hola</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Perferendis laudantium dicta quibusdam a animi, laboriosam doloribus dignissimos rerum molestiae aliquid libero, nemo, suscipit obcaecati commodi maxime totam ducimus! Eligendi, quae.</p>
+        </div>
+    )
+}
+
+export default Nosotros
